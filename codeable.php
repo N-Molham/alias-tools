9de6f099764773c7557feee1ebfe8dfb436be31f
Nabeel Molham <n.molham@gmail.com>
2017-03-11 15:52:49 +0200

    codeable estimate prompt

--- codeable.php
+++ codeable.php
@@ -1,37 +1,93 @@
 <?php
 
+// autoload packages
 require_once 'vendor/autoload.php';
 
+// Command Line tool initialization
 $cli = new League\CLImate\CLImate;
 
 // target estimate
 $estimate_best = isset( $argv[1] ) ? abs( floatval( $argv[1] ) ) : null;
 if ( empty( $estimate_best ) )
 {
-	// skip
-	dd( 'Invalid estimate input' );
+	// prompt message
+	$input = $cli->input( 'Your estimate:' );
+
+	// accept only positive float/int number
+	$input->accept( function ( $response )
+	{
+		$response = abs( floatval( $response ) );
+
+		return $response > 0;
+	} );
+
+	// prompt for amount
+	$estimate_best = $input->prompt();
 }
 
 // Excepted case scenario = estimate + 70%
-$estimate_excpected = $estimate_best + ( $estimate_best * 0.7 );
+$estimate_expected = $estimate_best + ( $estimate_best * 0.7 );
 
 // worst case scenario = will take 3 time needed time
 $estimate_worst = $estimate_best * 3;
 
-// final estimate formula
-$estimate_final = ( $estimate_best + ( 4 * $estimate_excpected ) + $estimate_worst ) / 6;
+// final estimate formula to add risk factor cost
+$estimate_final = ( $estimate_best + ( 4 * $estimate_expected ) + $estimate_worst ) / 6;
 
 // output
 $cli->lightGreenTable( [
-	[ 'Best Case', 		'B', 								amount( $estimate_best ) ],
-	[ 'Excepted Case', 	'EX = B + 70%', 					amount( $estimate_excpected ) ],
-	[ 'Worst Case', 	'W = 3 x B', 						amount( $estimate_worst ) ],
-	[ 'Final Case', 	'F = ( B + ( 4 x EX ) + W ) / 6', 	amount( $estimate_final ) ],
-	[ 'Client Amount', 	'F + 15%', 							amount( $estimate_final ) ],
-	[ 'Earned amount', 	'F - 10%', 							amount( $estimate_final ) ],
+	[
+		'Case',
+		'Var',
+		'Formula',
+		'Amount',
+	],
+	[
+		'Best Case',
+		'BC',
+		'BC',
+		amount( $estimate_best ),
+	],
+	[
+		'Excepted Case',
+		'EC',
+		'BC + 70%',
+		amount( $estimate_expected ),
+	],
+	[
+		'Worst Case',
+		'WC',
+		'3xBC',
+		amount( $estimate_worst ),
+	],
+	[
+		'Final Case',
+		'FC',
+		'( BC + 4xEC + WC ) / 6',
+		amount( $estimate_final ),
+	],
+	[
+		'Client Amount',
+		'CA',
+		'FC + 15%',
+		amount( $estimate_final ),
+	],
+	[
+		'Earned amount',
+		'EA',
+		'FC - 10%',
+		amount( $estimate_final ),
+	],
 ] );
 
-function amount( $amount ) 
+/**
+ * US$ Money format given amount
+ *
+ * @param int|float $amount
+ *
+ * @return string
+ */
+function amount( $amount )
 {
 	return sprintf( '$%s', number_format( $amount, 2 ) );
 }
